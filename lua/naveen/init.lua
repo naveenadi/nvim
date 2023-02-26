ec5db6ee28cf6c11edaef8e0b898bcc1b89674e4
Naveen Agarwal <naveenagarwal123451@gmail.com>
2023-02-26 23:26:33 +0530

    added missing lazy config and fix config

--- lua/naveen/init.lua
+++ lua/naveen/init.lua
@@ -1,13 +1,11 @@
-require "config.options"
-require "config.lazy"
+require 'naveen.config.options'
+require 'naveen.config.lazy'
 
-[[
-vim.api.nvim_create_autocmd("User", {
-  pattern = "VeryLazy",
+-- autocmds and keymaps can wait to load
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'VeryLazy',
   callback = function()
-    require "config.autocmds"
-    require "config.keymaps"
+    require 'naveen.config.autocmds'
+    require 'naveen.config.keymaps'
   end,
 })
-]]
-
