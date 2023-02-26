e6bfa81cf70f2b094021e9b09bb8376677a64af3
Naveen Agarwal <naveenagarwal123451@gmail.com>
2023-02-26 21:37:54 +0530

    options added

---
+++ lua/naveen/init.lua
@@ -0,0 +1,13 @@
+require "config.options"
+require "config.lazy"
+
+[[
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    require "config.autocmds"
+    require "config.keymaps"
+  end,
+})
+]]
+
