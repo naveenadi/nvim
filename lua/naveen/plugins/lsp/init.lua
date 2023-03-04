2e18a73a859f0df8b991d6f14596f5a3147d12c3
Naveen Agarwal <naveenagarwal123451@gmail.com>
2023-03-05 00:44:32 +0530

    added basic lsp setup

---
+++ lua/naveen/plugins/lsp/init.lua
@@ -0,0 +1,114 @@
+return {
+  -- cmdline tools and lsp servers
+  {
+    'williamboman/mason.nvim',
+    cmd = 'Mason',
+    keys = { { '<leader>cm', '<cmd>Mason<cr> ' } },
+    opts = {
+      ensure_installed = {
+        'stylua',
+        'shellcheck',
+        'shfmt',
+        'flake8',
+      },
+    },
+
+    ---@param opts MasonSettings | {ensure_installed: string[]}
+    config = function(_, opts)
+      require('mason').setup(opts)
+      local mr = require 'mason-registry'
+      for _, tool in ipairs(opts.ensure_installed) do
+        local p = mr.get_package(tool)
+        if not p:is_installed() then
+          p:install()
+        end
+      end
+    end,
+  },
+
+  -- lspconfig
+  {
+    'neovim/nvim-lspconfig',
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = {
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+    },
+    ---@class PluginLspOpts
+    opts = {
+      servers = {
+        jsonls = {},
+        lua_ls = {
+          settings = {
+            Lua = {
+              completion = { callSnippet = 'Replace' },
+              diagnostics = { globals = { 'vim' } },
+              workspace = { checkThirdParty = false },
+            },
+          },
+        },
+      },
+      -- you can do any additional lsp server setup here
+      -- return true if you don't want this server to be setup with lspconfig
+      ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+      setup = {
+        -- example to setup with typescript.nvim
+        -- tsserver = function(_, opts)
+        --   require("typescript").setup({ server = opts })
+        --   return true
+        -- end,
+        -- Specify * to use this function as a fallback for any server
+        -- ["*"] = function(server, opts) end,
+      },
+    },
+    ---@param opts PluginLspOpts
+    config = function(plugin, opts)
+      local servers = opts.servers
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+      local function setup(server)
+        local server_opts = vim.tbl_deep_extend('force', {
+          capabilities = vim.deepcopy(capabilities),
+        }, servers[server] or {})
+
+        if opts.setup[server] then
+          if opts.setup[server](server, server_opts) then
+            return
+          end
+        elseif opts.setup['*'] then
+          if opts.setup['*'](server, server_opts) then
+            return
+          end
+        end
+        require('lspconfig')[server].setup(server_opts)
+      end
+
+      -- temp fix for lspconfig rename
+      -- https://github.com/neovim/nvim-lspconfig/pull/2439
+      local mappings = require 'mason-lspconfig.mappings.server'
+      if not mappings.lspconfig_to_package.lua_ls then
+        mappings.lspconfig_to_package.lua_ls = 'lua-language-server'
+        mappings.package_to_lspconfig['lua-language-server'] = 'lua_ls'
+      end
+
+      local mlsp = require 'mason-lspconfig'
+      local available = mlsp.get_available_servers()
+
+      local ensure_installed = {} ---@type string[]
+      for server, server_opts in pairs(servers) do
+        if server_opts then
+          server_opts = server_opts == true and {} or server_opts
+          -- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
+          if server_opts.mason == false or not vim.tbl_contains(available, server) then
+            setup(server)
+          else
+            ensure_installed[#ensure_installed + 1] = server
+          end
+        end
+      end
+
+      require('mason-lspconfig').setup { ensure_installed = ensure_installed }
+      require('mason-lspconfig').setup_handlers { setup }
+    end,
+  },
+}
